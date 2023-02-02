3b90123c60d7db4828773df66c215b893484ea8f
Yourmercury <momohpolycarp@gmail.com>
2023-02-02 05:33:59 +0100

    Added stream counting feature

--- src/components/Video/bigPlay.jsx
+++ src/components/Video/bigPlay.jsx
@@ -1,10 +1,10 @@
 export default function BigPlay({ play }) {
   return (
     <div className="flex px-[4vw] absolute centered w-full">
-      <div className="text-white w-[30%] max-h-[30vh] overflow-y-hidden bg-[#0000003e] p-[10px]">
+      {/* <div className="text-white w-[30%] max-h-[30vh] overflow-y-hidden bg-[#0000003e] p-[10px]">
         How to turn your songs to NFT on RadioVaults How to turn your songs to
         NFT on RadioVaults How to turn your songs to NFT on RadioVaults
-      </div>
+      </div> */}
       <div className="flex-1 flex justify-center items-center">
         <img
           src="/assets/big-play.svg"
