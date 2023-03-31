2735f6a85d816c7d0707461a8996f2ed5c57926e
Yourmercury <momohpolycarp@gmail.com>
2023-03-31 22:48:21 +0100

    cover Photo added and compulsory

--- src/components/uploadImage.jsx
+++ src/components/uploadImage.jsx
@@ -1,7 +1,7 @@
 export default function UploadMedia({imageFile, setImageFile, setCoverImage, coverImage}){
     return (
         <div className="m-auto flex">
-            <label className="flex flex-col justify-center mx-auto items-center border w-fit p-10 rounded-xl border-dashed border-[3px] border-theme-grey bg-theme-light-grey">
+            <label className="flex flex-col justify-center mx-auto items-center w-fit p-10 rounded-xl border-dashed border-[3px] border-theme-grey bg-theme-light-grey">
                 {/* <span className="text-[12px]">Drag and Drop Video</span> */}
                 {coverImage && <span className="text-theme-red text-[12px]">{coverImage.name}</span>}
                 {/* {coverImage && <img src={coverImage.} alt="" /> */}
@@ -12,7 +12,7 @@ export default function UploadMedia({imageFile, setImageFile, setCoverImage, cov
                     value[0] && setCoverImage(value[0]);
                 }}/>
             </label>
-            <label className="ml-10 flex flex-col justify-center mx-auto items-center border w-fit p-10 rounded-xl border-dashed border-[3px] border-theme-grey bg-theme-light-grey">
+            <label className="ml-10 flex flex-col justify-center mx-auto items-center w-fit p-10 rounded-xl border-dashed border-[3px] border-theme-grey bg-theme-light-grey">
                 {/* <span className="text-[12px]">Drag and Drop Video</span> */}
                 {imageFile && <span className="text-theme-red text-[12px]">{imageFile.name}</span>}
                 {!imageFile && <><span className="text-[12px]">Click here to</span>
--- src/pages/api/metadata/[id].js
+++ src/pages/api/metadata/[id].js
@@ -1,7 +1,30 @@
 import db from "@/database/db";
 import { _getMetadata, maskStream, parseUrl } from "@/utils";
+import Cors from "cors";
+
+const cors = Cors({
+  method: ["GET"]
+})
+
+function runMiddleware(
+  req,
+  res,
+  fn
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
 
 export default async function handler(req, res) {
+    await runMiddleware(req, res, cors);
     try {
         let id = req.query.id;
         let client = req.headers.referer || req.headers.host;
